50d6d816af6270eef0913ed255cbc5c8ac3047cc
candytangban <tangbanllniu@163.com>
2020-11-18 15:07:05 +0800

    save fig dir

--- simplelayout/__main__.py
+++ simplelayout/__main__.py
@@ -8,8 +8,8 @@ def main():
     generated_matrix = core.generate_matrix(positions_params.board_grid, positions_params.unit_grid,
                                             positions_params.unit_n, positions_params.positions)
     utils.make_dir(positions_params.outdir)
-    utils.save_matrix(generated_matrix, positions_params.outdir + positions_params.file_name)
-    utils.save_fig(generated_matrix, positions_params.outdir + positions_params.file_name)
+    utils.save_matrix(generated_matrix, positions_params.outdir + '/' + positions_params.file_name)
+    utils.save_fig(generated_matrix, positions_params.outdir + '/' + positions_params.file_name)
 
 
 if __name__ == "__main__":
