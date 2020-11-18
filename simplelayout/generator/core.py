d080aa9889cdea7e77400b9e04f7e2def5491db7
candytangban <tangbanllniu@163.com>
2020-11-18 23:28:53 +0800

    kron operator

--- simplelayout/generator/core.py
+++ simplelayout/generator/core.py
@@ -17,10 +17,18 @@ def generate_matrix(
     Returns:
         np.ndarray: 布局矩阵
     """
-    random_matrix = np.zeros((board_grid, board_grid))
-    temp = (board_grid / unit_grid)
+    block_num = int(board_grid / unit_grid)
+    random_matrix_block = np.zeros((block_num, block_num))
+    random_matrix_reshape = random_matrix_block.reshape(1, -1)
     for i in range(unit_n):
-        temp_x = int((positions[i] - 1) // temp * temp)
-        temp_y = int((positions[i] - 1) % temp * temp)
-        random_matrix[temp_x:temp_x + unit_grid, temp_y:temp_y + unit_grid] = 1
+        random_matrix_reshape[0, positions[i] - 1] = 1
+    random_matrix_reverse = random_matrix_reshape.reshape(block_num, block_num)
+
+    random_matrix = np.kron(random_matrix_reverse, np.ones((unit_grid, unit_grid)))
+    # random_matrix = np.zeros((board_grid, board_grid))
+    # temp = (board_grid / unit_grid)
+    # for i in range(unit_n):
+    #     temp_x = int((positions[i] - 1) // temp * temp)
+    #     temp_y = int((positions[i] - 1) % temp * temp)
+    #     random_matrix[temp_x:temp_x + unit_grid, temp_y:temp_y + unit_grid] = 1
     return random_matrix
--- simplelayout/generator/utils.py
+++ simplelayout/generator/utils.py
@@ -15,7 +15,7 @@ def save_matrix(matrix, file_name):
 def save_fig(matrix, file_name):
     plt.imshow(matrix)
     plt.savefig(file_name + '.jpg')
-    plt.show()
+    # plt.show()
 
 
 def make_dir(outdir):
