9caa18c39880aff813e193e7bfc0674b80feabc8
candytangban <tangbanllniu@163.com>
2020-11-18 23:36:46 +0800

    kron operator

--- simplelayout/generator/core.py
+++ simplelayout/generator/core.py
@@ -24,11 +24,13 @@ def generate_matrix(
         random_matrix_reshape[0, positions[i] - 1] = 1
     random_matrix_reverse = random_matrix_reshape.reshape(block_num, block_num)
 
-    random_matrix = np.kron(random_matrix_reverse, np.ones((unit_grid, unit_grid)))
+    random_matrix = np.kron(random_matrix_reverse,
+                            np.ones((unit_grid, unit_grid)))
     # random_matrix = np.zeros((board_grid, board_grid))
     # temp = (board_grid / unit_grid)
     # for i in range(unit_n):
     #     temp_x = int((positions[i] - 1) // temp * temp)
     #     temp_y = int((positions[i] - 1) % temp * temp)
-    #     random_matrix[temp_x:temp_x + unit_grid, temp_y:temp_y + unit_grid] = 1
+    #     random_matrix[temp_x:temp_x + unit_grid,
+    #                   temp_y:temp_y + unit_grid] = 1
     return random_matrix
